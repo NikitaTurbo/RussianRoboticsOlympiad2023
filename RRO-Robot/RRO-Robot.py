eb72a3ad491e8a978a58b198293b77e255f1c577
Nikita Turbo <nikita.turbo.2011@gmail.com>
2023-04-21 07:56:38 +0300

    rro

---
+++ RRO-Robot/RRO-Robot.py
@@ -0,0 +1,23 @@
+import sys
+
+d1=brick.sensor("D1")
+d2=brick.sensor("D2")
+
+while True:
+    distance_sensor_r=d1.read()   
+    distance_sensor_l=d2.read()
+    brick.display().setBackground ("white")
+    brick.display().setPainterColor ("black")
+    brick.display().setPainterWidth(100)
+    brick.display().redraw()
+    brick.display().addLabel("Left: " + str(distance_sensor_l), 1, 1)
+    brick.display().addLabel("Right: " + str(distance_sensor_r), 1, 20)
+    script.wait(100)
+    
+if mailbox.hasMessages() == True:
+    brick.display().setBackground ("white")
+    brick.display().setPainterColor ("black")
+    brick.display().setPainterWidth(100)
+    brick.display().redraw()
+    brick.display().addLabel(mailbox.receive(), 1, 1)
+    
---
+++ RRO-Robot/test.py
@@ -0,0 +1,6 @@
+import sys
+
+mailbox.connect("192.168.77.204")
+mailbox.send(2, "Hello")
+ 
+script.wait(10000)
